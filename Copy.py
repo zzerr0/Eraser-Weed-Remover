7d83f8aed6d2c966b2852c01045a112b10ecc902
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-11-01 11:39:54 +0530

    Create Copy.py

---
+++ Copy.py
@@ -0,0 +1,5 @@
+import shutil
+
+for i in range(100):
+    shutil.copy2('/root/Desktop/0.jpeg', '/root/Desktop/ZERO/data/test/weed3/weed1_{}.jpeg'.format(i))  
+    #             dESTINATION from                       destination to
